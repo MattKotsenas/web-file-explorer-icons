a1a97875597754daf03c60489a16a8dde92dc591
Matt Kotsenas <matt.kotsenas@gmail.com>
2025-05-14 00:50:14 -0700

    works, but glitchy on refresh

--- src/sites/azuredevops.ts
+++ src/sites/azuredevops.ts
@@ -3,7 +3,7 @@ import type { ReplacementSelectorSet, Site } from './index.js';
 const mainRepositoryImplementation: ReplacementSelectorSet = {
 	row: 'table[aria-label="Files table"] tr td:nth-of-type(2)',
 	filename: 'a',
-	icon: 'span.fabric-icon',
+	icon: '.fabric-icon',
 	isDirectory: (_rowEl, _fileNameEl, iconEl) =>
 		iconEl.classList.contains('repos-folder-icon'),
 	isSubmodule: (_rowEl, _fileNameEl, iconEl) =>
@@ -12,9 +12,9 @@ const mainRepositoryImplementation: ReplacementSelectorSet = {
 };
 
 const repositorySideTreeImplementation: ReplacementSelectorSet = {
-	row: 'table[aria-label="File explorer tree"] tr td:nth-of-type(2) div.repos-file-explorer-tree-cell div.text-ellipsis',
+	row: 'table[aria-label="File explorer tree"] tr td:nth-of-type(2) .repos-file-explorer-tree-cell',
 	filename: 'a',
-	icon: 'span.fabric-icon',
+	icon: '.fabric-icon:not(.bolt-tree-expand-button)',
 	isDirectory: (_rowEl, _fileNameEl, iconEl) =>
 		iconEl.classList.contains('repos-folder-icon'),
 	isSubmodule: (_rowEl, _fileNameEl, iconEl) =>
