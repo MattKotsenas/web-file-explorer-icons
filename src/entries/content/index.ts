b9036ae0efc1051e60143c0f75c6929f26287ce1
Matt Kotsenas <matt.kotsenas@gmail.com>
2025-05-15 23:52:34 -0700

    Trying things; doesn't work

--- src/entries/content/index.ts
+++ src/entries/content/index.ts
@@ -1,6 +1,6 @@
 import { defineContentScript } from 'wxt/sandbox';
 
-import { observe } from 'selector-observer';
+// import { observe } from 'selector-observer';
 
 import { type ReplacementSelectorSet, matches, sites } from '@/sites';
 import { flavor } from '@/storage';
@@ -34,15 +34,71 @@ function runReplacements(
 	stylesEl: Element,
 ) {
 	// Monitor DOM elements that match a CSS selector.
-	for (const replacement of replacements) {
-		observe(replacement.row, {
-			async add(rowEl: HTMLElement) {
-				await replaceIconInRow(rowEl, replacement);
-			},
-		});
-	}
+	whenReady(document, () => {
+		for (const replacement of replacements) {
+			observeSelector(replacement.root, replacement.row, (el) => {
+				replaceIconInRow(el, replacement);
+			});
+			// observe(replacement.row, {
+			// 	async add(rowEl: HTMLElement) {
+			// 		await replaceIconInRow(rowEl, replacement);
+			// 	},
+			// });
+		}
+	});
 
 	const rawStyles = replacements.map(({ styles }) => styles || '').join('\n');
 	flavor.watch(() => injectStyles(stylesEl, rawStyles));
 	injectStyles(stylesEl, rawStyles);
+}
+
+function observeSelector(root: string, selector: string, callback) {
+  const observer = new MutationObserver((mutationsList) => {
+    for (const mutation of mutationsList) {
+		mutation.addedNodes.forEach((node) => {
+			if (node instanceof HTMLElement) {
+				const match = node.closest(selector);
+				if (match !== null) {
+					callback(match);
+				}
+			}
+			// for (const el of queryAllInSubtree(node, selector)) {
+			// 	callback(el);
+			// }
+			// if (node instanceof Element && node.matches(selector)) {
+			//   callback(node);
+			// }
+      });
+
+	  if (mutation.target instanceof Element) {
+			const match = mutation.target.closest(selector);
+			if (match !== null) {
+				callback(match);
+			}
+	  }
+    }
+  });
+
+  observer.observe(document.body, {
+    childList: true,
+    subtree: true,
+  });
+}
+
+function whenReady(document: Document, callback) {
+  const readyState = document.readyState
+  if (readyState === 'interactive' || readyState === 'complete') {
+    callback()
+  } else {
+    document.addEventListener('DOMContentLoaded', callback)
+  }
+}
+
+function queryAllInSubtree(subtreeRoot, selector) {
+  return Array.from(document.querySelectorAll(selector))
+              .filter(el => subtreeRoot.contains(el));
+}
+
+function isUnderSelector(node: HTMLElement, selector: string) {
+  return node.closest(selector);
 }
--- src/sites/azuredevops.ts
+++ src/sites/azuredevops.ts
@@ -1,9 +1,12 @@
 import type { ReplacementSelectorSet, Site } from './index.js';
 
+// TODO: The root idea probably doesn't work.
+
 const mainRepositoryImplementation: ReplacementSelectorSet = {
-	row: 'table[aria-label="Files table"] tr td:nth-of-type(2)',
-	filename: 'a',
-	icon: '.fabric-icon',
+	root: 'table[aria-label="Files table"]',
+	row: 'table[aria-label="Files table"] tr',
+	filename: 'td:nth-of-type(2) a',
+	icon: 'td:nth-of-type(2) .fabric-icon',
 	isDirectory: (_rowEl, _fileNameEl, iconEl) =>
 		iconEl.classList.contains('repos-folder-icon'),
 	isSubmodule: (_rowEl, _fileNameEl, iconEl) =>
@@ -12,25 +15,16 @@ const mainRepositoryImplementation: ReplacementSelectorSet = {
 };
 
 const repositorySideTreeImplementation: ReplacementSelectorSet = {
-	row: 'table[aria-label="File explorer tree"] tr td:nth-of-type(2) .repos-file-explorer-tree-cell',
-	filename: 'a',
-	icon: '.fabric-icon:not(.bolt-tree-expand-button)',
+	root: 'table[aria-label="File explorer tree"]',
+	row: 'table[aria-label="File explorer tree"] tr',
+	filename: 'td:nth-of-type(2) .repos-file-explorer-tree-cell a',
+	icon: 'td:nth-of-type(2) .repos-file-explorer-tree-cell .fabric-icon:not(.bolt-tree-expand-button)',
 	isDirectory: (_rowEl, _fileNameEl, iconEl) =>
 		iconEl.classList.contains('repos-folder-icon'),
 	isSubmodule: (_rowEl, _fileNameEl, iconEl) =>
 		iconEl.classList.contains('repos-submodule'),
 	isCollapsable: (_rowEl, _fileNameEl, _iconEl) =>
 		repositorySideTreeImplementation.isDirectory(_rowEl, _fileNameEl, _iconEl),
-};
-
-const directoryContentsHeaderImplementation: ReplacementSelectorSet = {
-	row: 'div:has(+ [data-qa="repository-directory"]), div:has(+ .rah-static [data-qa="repository-directory"])',
-	filename:
-		'div:has(span[aria-label="Directory,"] svg) + span > span:last-of-type',
-	icon: 'span[aria-label="Directory,"] svg',
-	isDirectory: (_rowEl, _fileNameEl, _iconEl) => true,
-	isSubmodule: (_rowEl, _fileNameEl, _iconEl) => false,
-	isCollapsable: (_rowEl, _fileNameEl, _iconEl) => false,
 };
 
 export const azuredevops: Site = {
